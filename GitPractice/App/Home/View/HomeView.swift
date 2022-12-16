64df92996f2895611b065cab8e1ad0a1f950fd3c
Sazid Saifi <sazidsaifi1998@gmail.com>
2022-12-16 16:55:08 +0530

    Add Sazid

--- GitPractice/App/Home/View/HomeView.swift
+++ GitPractice/App/Home/View/HomeView.swift
@@ -13,7 +13,7 @@ struct HomeView: View {
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             Text("Initial commit")
             Text("Add New line")
-
+            Text("Sazid")
         }
     }
 }
