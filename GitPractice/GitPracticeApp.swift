8171ca84e0a57d3a2638818695566f46bb87d326
Hemant Kumar <hmntkmr952807@gmail.com>
2022-12-16 15:42:20 +0530

    Initial Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Hemant on 16/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
