d7e761bcc37e26cb17edbd256b1a8e71fed5e50c
Hemant Kumar <hmntkmr952807@gmail.com>
2022-12-16 15:45:23 +0530

    initial commit

---
+++ GitPractice/App/Home/View/HomeView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeView.swift
+//  GitPractice
+//
+//  Created by Hemant on 16/12/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("Initial commit")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
